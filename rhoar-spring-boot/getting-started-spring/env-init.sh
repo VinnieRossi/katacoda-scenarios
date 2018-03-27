fec79ede170e604d2f0bcc1321d720f6103b0712
Edge, Matthew <mattedgeis@gmail.com>
2018-03-27 18:22:18 -0400

    Removing the ssh step

--- rhoar-spring-boot/getting-started-spring/env-init.sh
+++ rhoar-spring-boot/getting-started-spring/env-init.sh
@@ -1,1 +1,1 @@
-ssh root@client "git clone https://github.com/GetLevvel/rhoar-sample-code.git && yum install tree -y"
+git clone https://github.com/GetLevvel/rhoar-sample-code.git && yum install tree -y
