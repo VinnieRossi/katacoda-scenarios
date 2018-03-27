528cd2dd18debcbe7af9cf03eb82d85c37aaeaa6
Edge, Matthew <mattedgeis@gmail.com>
2018-03-27 11:19:04 -0400

    Trying to revert to set-env.sh with a different order

--- rhoar-spring-boot/getting-started-spring/env-init.sh
+++ rhoar-spring-boot/getting-started-spring/env-init.sh
@@ -1,2 +1,1 @@
-ssh root@host01 "git clone https://github.com/GetLevvel/rhoar-sample-code.git"
 ssh root@host01 "yum install tree -y"
--- rhoar-spring-boot/getting-started-spring/set-env.sh
+++ rhoar-spring-boot/getting-started-spring/set-env.sh
@@ -1,2 +1,3 @@
+git clone https://github.com/GetLevvel/rhoar-sample-code.git
 ~/.launch.sh
 cd /root/rhoar-sample-code/getting-started-spring
