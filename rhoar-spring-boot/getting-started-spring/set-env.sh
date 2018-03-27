dfc569869eaac7f25a69cdac6ad9b12f0405ad3a
Edge, Matthew <mattedgeis@gmail.com>
2018-03-27 18:33:44 -0400

    Removing git target path

--- rhoar-spring-boot/getting-started-spring/set-env.sh
+++ rhoar-spring-boot/getting-started-spring/set-env.sh
@@ -1,3 +1,3 @@
-mkdir -p /root/code && git clone -q https://github.com/GetLevvel/rhoar-sample-code.git /root/code
+mkdir -p /root/code && git clone -q https://github.com/GetLevvel/rhoar-sample-code.git
 cd /root/code/getting-started-spring && cp -R /root/code/rhoar-sample-code/getting-started-spring/* ./
 ~/.launch.sh
