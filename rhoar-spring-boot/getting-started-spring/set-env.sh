6d2525fef13365e2a2280152b5d9c223969a9e15
Edge, Matthew <mattedgeis@gmail.com>
2018-03-27 18:09:50 -0400

    set-env rework

--- rhoar-spring-boot/getting-started-spring/set-env.sh
+++ rhoar-spring-boot/getting-started-spring/set-env.sh
@@ -1,4 +1,3 @@
-cd /root/code
-git clone https://github.com/GetLevvel/rhoar-sample-code.git
+cd /root/code && git clone https://github.com/GetLevvel/rhoar-sample-code.git .
 ~/.launch.sh
 cd /root/code/getting-started-spring
