b51feebfa8ce7585c52b90435b0bb351374f3fd8
Edge, Matthew <mattedgeis@gmail.com>
2018-03-27 18:31:02 -0400

    More workarounds if user clicks too fast

--- rhoar-spring-boot/getting-started-spring/set-env.sh
+++ rhoar-spring-boot/getting-started-spring/set-env.sh
@@ -1,4 +1,6 @@
-cd /root/code && git clone https://github.com/GetLevvel/rhoar-sample-code.git
+mkdir -p /root/code 
+cd /root/code
+git clone https://github.com/GetLevvel/rhoar-sample-code.git
 ~/.launch.sh
 cd /root/code/getting-started-spring
 cp -R /root/code/rhoar-sample-code/getting-started-spring/* ./
