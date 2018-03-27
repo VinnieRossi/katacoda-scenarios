43717ffab3d709de6a6cff6bbacd931646f8ada6
Edge, Matthew <mattedgeis@gmail.com>
2018-03-27 10:43:28 -0400

    Moving more steps to set-env.sh since this seems to actually work

--- rhoar-spring-boot/getting-started-spring/set-env.sh
+++ rhoar-spring-boot/getting-started-spring/set-env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /root/projects/getting-started-spring
+cd /root/projects/
 git clone https://github.com/GetLevvel/rhoar-sample-code.git .
+cd /root/projects/getting-started-spring
 ~/.launch.sh
 
