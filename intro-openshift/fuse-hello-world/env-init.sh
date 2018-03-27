187ec0682e1d939ef24e494f86d8e31b99d12d10
Edge, Matthew <mattedgeis@gmail.com>
2018-03-27 09:40:37 -0400

    Deleting intro to openshift content

--- intro-openshift/fuse-hello-world/env-init.sh
+++
@@ -1,4 +0,0 @@
-#ssh root@host01 "docker pull registry.access.redhat.com/jboss-fuse-6/fis-java-openshift:latest"
-ssh root@host01 'until $(oc status &> /dev/null); do sleep 1; done && sleep 5 && (oc login -u system:admin >> /opt/logs && oc project openshift >> /opt/logs && oc create -f https://raw.githubusercontent.com/jboss-fuse/application-templates/master/fis-image-streams.json >> /opt/logs)'
-
-
