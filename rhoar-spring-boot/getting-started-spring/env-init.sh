345ba70420d7aec9452c24e692e02d5a5f633882
Edge, Matthew <mattedgeis@gmail.com>
2018-03-27 10:57:05 -0400

    Take 3 on env-init

--- rhoar-spring-boot/getting-started-spring/env-init.sh
+++ rhoar-spring-boot/getting-started-spring/env-init.sh
@@ -1,1 +1,2 @@
+ssh root@host01 "cd /root/projects && git clone https://github.com/GetLevvel/rhoar-sample-code.git"
 ssh root@host01 "yum install tree -y"
--- rhoar-spring-boot/getting-started-spring/set-env.sh
+++ rhoar-spring-boot/getting-started-spring/set-env.sh
@@ -1,5 +1,3 @@
-cd /root/scenario-code
-git clone https://github.com/GetLevvel/rhoar-sample-code.git .
-cd /root/scenario-code/getting-started-spring
+cd /root/scenario-code/rhoar-sample-code/getting-started-spring
 ~/.launch.sh
 
