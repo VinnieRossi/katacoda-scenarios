fce9bcd78a11926e58e3f39b2a4f29b025b1a883
Edge, Matthew <mattedgeis@gmail.com>
2018-03-28 10:36:29 -0400

    Standardizing DB Access module with new env-init steps

---
+++ rhoar-spring-boot/spring-db-access/env-init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Standard environment initialization script. Assumes the installation path (the cp portion) has been
+# created by Katacoda via a environment.uieditorpath key. (ex: "uieditorpath": "/root/code/spring-mvc")
+
+PROJECT=spring-db-access	# The name of the folder within the code samples repo to copy
+UI_PATH=/root/code 	  		# This should match your index.json key
+
+git clone -q https://github.com/GetLevvel/rhoar-sample-code.git
+cd ${UI_PATH} && cp -R /root/rhoar-sample-code/${PROJECT}/* ./
+~/.launch.sh
--- rhoar-spring-boot/spring-db-access/set-env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git clone -q https://github.com/GetLevvel/rhoar-sample-code.git
-cd /root/code/spring-db-access && cp -R /root/rhoar-sample-code/spring-db-access/* ./
-~/.launch.sh
