bc5604facd75384725210194faaf707f8317ab23
Edge, Matthew <mattedgeis@gmail.com>
2018-03-28 10:24:53 -0400

    Assets folder not accessible. Using as template instead

--- rhoar-spring-boot/getting-started-spring/env-init.sh
+++ rhoar-spring-boot/getting-started-spring/env-init.sh
@@ -2,8 +2,8 @@
 # Standard environment initialization script. Assumes the installation path (the cp portion) has been
 # created by Katacoda via a environment.uieditorpath key. (ex: "uieditorpath": "/root/code/spring-mvc")
 
-PROJECT=TODO			  # the name of the folder within the code samples repo to copy
-UI_PATH=/root/code 		  # This should match your index.json key
+PROJECT=getting-started-spring  # The name of the folder within the code samples repo to copy
+UI_PATH=/root/code 				# This should match your index.json key
 
 git clone -q https://github.com/GetLevvel/rhoar-sample-code.git
 cd ${UI_PATH} && cp -R /root/rhoar-sample-code/${PROJECT}/* ./
--- rhoar-spring-boot/getting-started-spring/set-env.sh
+++
@@ -1,3 +0,0 @@
-git clone -q https://github.com/GetLevvel/rhoar-sample-code.git
-cd /root/code/getting-started-spring && cp -R /root/rhoar-sample-code/getting-started-spring/* ./
-~/.launch.sh
