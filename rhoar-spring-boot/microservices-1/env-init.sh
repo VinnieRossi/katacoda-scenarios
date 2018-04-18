58c12e9d8130db091ed269558ed4225fc036c854
Vinnie <vinnie.rossi12@gmail.com>
2018-04-18 13:45:37 -0400

    point project to test code

--- rhoar-spring-boot/microservices-1/env-init.sh
+++ rhoar-spring-boot/microservices-1/env-init.sh
@@ -2,7 +2,7 @@
 # Standard environment initialization script. Assumes the installation path (the cp portion) has been
 # created by Katacoda via a environment.uieditorpath key. (ex: "uieditorpath": "/root/code/spring-mvc")
 
-PROJECT=spring-microservices  # The name of the folder within the code samples repo to copy
+PROJECT=test-config  # The name of the folder within the code samples repo to copy
 UI_PATH=/root/code 			  # This should match your index.json key
 
 git clone -q https://github.com/GetLevvel/rhoar-sample-code.git
